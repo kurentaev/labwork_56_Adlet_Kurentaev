addf2b895d6827fbcb74db2e2c1c504391f093e1
Adlet Kurentaev <kurentaev@gmail.com>
2022-10-03 11:47:49 +0600

    Добавлено добавление товаров

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from webapp.views.base import index_view
 from webapp.views.products import product_view
-# from webapp.views.guests import add_view
+from webapp.views.products import add_view
 # from webapp.views.guests import update_view
 # from webapp.views.guests import delete_view
 # from webapp.views.guests import confirm_delete
@@ -12,7 +12,7 @@ urlpatterns = [
     path('', index_view, name='index'),
     path('products/', index_view, name='index_view'),
     path('products/<int:pk>', product_view, name='product_detail'),
-    # path('guest_book/add/', add_view, name='guest_add'),
+    path('products/add/', add_view, name='product_add'),
     # path('guest_book/update/<int:pk>', update_view, name='guest_update'),
     # path('guest_book/delete/<int:pk>', delete_view, name='guest_delete'),
     # path('guest_book/confirm_delete/<int:pk>', confirm_delete, name='confirm_delete')
--- source/webapp/views/products.py
+++ source/webapp/views/products.py
@@ -2,10 +2,29 @@ from django.shortcuts import render, redirect, get_object_or_404
 
 from webapp.models import ProductsList
 from webapp.models import StatusChoices
-# from webapp.forms import TasksListForm
+from webapp.forms import ProductsListForm
 
 
 def product_view(request, pk):
     product = get_object_or_404(ProductsList, pk=pk)
     return render(request, 'product.html', context={'product': product, "choices": StatusChoices.choices})
+
+
+def add_view(request):
+    if request.method == "GET":
+        form = ProductsListForm()
+        return render(request, "product_add.html", context={'form': form})
+    elif request.method == "POST":
+        form = ProductsListForm(request.POST)
+        if form.is_valid():
+            product_data = {
+                'title': request.POST.get('title'),
+                'description': request.POST.get('description'),
+                'image': request.POST.get('image'),
+                'category': request.POST.get('category'),
+                'rest': request.POST.get('rest'),
+                'price': request.POST.get('price'),
+            }
+            product = ProductsList.objects.create(**product_data)
+            return redirect('product_detail', pk=product.pk)
 
